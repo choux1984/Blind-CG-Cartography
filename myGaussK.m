b867c7440c1e673021dddae1a3aba17e441cd5d7
Donghoon Lee <leex6962@umn.edu>
2016-08-25 12:02:49 -0500

    Revert "Synchronize with OOP "
    This reverts commit a26a4a97816c4759719395d7408f3e4117608777.

--- myGaussK.m
+++
@@ -1,5 +0,0 @@
-function outK = myGaussK(var,input,parameter)
-    % dist := Distance between input and parameter
-    dist = norm(input - parameter,2);
-    outK = exp(-(2 * var)^(-1) * dist^2);
-end
